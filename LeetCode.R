72d8eee5340c64d5524f67b7e07c114a3ea1ce61
JackOJordan <132203912+JackOJordan@users.noreply.github.com>
2023-07-07 15:29:39 +0100

    Problems 1-19

--- LeetCode.R
+++ LeetCode.R
@@ -1,15 +1,15 @@
-# Problem 1 - Two Sum
+# Problem 1 - Two Sum - Under one second
 
-nums <- c(2,3,4,5,6,7,8,9,10)
-target <- 18
+nums <- c(1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10,11,12)
+target <- 23
 for(i in 1:length(nums)){
   test <- nums[i]
   if((target - nums[i]) %in% nums[-i]) {print(c(i, which(nums == (target - nums[i])))); break}
 }
 
-# Problem 2 - Add Two Numbers
+# Problem 2 - Add Two Numbers - Under one second
 
-l1 <- c(8,4,6,2,5,9,0,6,4,3); l2 <- c(5,8,3,5,7,5,0,4,3)
+l1 <- c(5, 2, 5, 0, 8, 5, 1, 5, 6, 1, 3, 3, 4, 1, 2, 9, 4, 5, 9, 6, 7, 4, 7, 4, 0, 1, 8, 1, 2, 5, 2, 8, 4, 0, 8, 5, 0, 4, 0, 6, 0, 2, 2, 8, 3, 0, 7, 1, 2, 7, 9, 8, 5, 4, 3, 2, 3, 6, 0, 3, 5, 1, 1, 7, 0, 6, 4, 0, 4, 6, 3, 6, 9, 7, 2, 7, 3, 5, 6, 9, 5, 2, 7, 9, 0, 0, 1, 7, 1, 5, 6, 8, 1, 9, 7, 5, 0, 6, 7, 2); l2 <- c(0, 3, 1, 2, 8, 7, 2, 7, 5, 6, 3, 0, 9, 7, 5, 1, 2, 9, 1, 3, 6, 6, 5, 1, 7, 0, 4, 8, 0, 7, 4, 7, 2, 1, 2, 6, 5, 1, 7, 5, 7, 3, 6, 9, 2, 5, 5, 0, 7, 4, 9, 7, 7, 2, 9, 4, 7, 2, 7, 8, 4, 5, 3, 5, 2, 8, 3, 3, 0, 8, 1, 3, 2, 6, 9, 2, 9, 0, 4, 0, 3, 7, 9, 3, 9, 3, 4, 3, 3, 9, 5, 8, 7, 6, 5, 7, 9, 8, 4, 7)
 output <- c()
 carry <- 0
 for(i in 1:max(c(length(l1), length(l2)))){
@@ -27,35 +27,53 @@ for(i in 1:max(c(length(l1), length(l2)))){
 }
 output
 
-# Problem 3 - Longest Substring Without Repeating Characters
+# Problem 3 - Longest Substring Without Repeating Characters - Under one second
 
-s <- "abcabcdklasbdlbsdlnvslsdbghshdfkhwlubdvhlkvshsdlvblsabdoglbrgljbjdgbbb"
+# s <- "abcabcdklasbdlbsdlnvslsdbghshdfkhwlubdvhlkvshsdlvblsabdoglbrgljbjdgbbb"
 
 # Note: Will need to change some stuff if the character \ appears in the string.
 
-t <- unlist(strsplit(s, split = ""))
+setwd("C:/Users/jackj.LAPTOP-U1V11TR1/Documents/R Projects")
+stringConnect <- file(description = "LeetCode3.txt", open = "r", blocking = TRUE)
+string <- readLines(stringConnect)
 
+t <- unlist(strsplit(string, split = ""))
+
+l <- length(t)
 highestLength <- 1
 index <- c()
 test <- c()
-for(j in 1:length(t)){
-  if((length(t) - j) <= highestLength) break
+for(j in 1:l){
+  if((l - j) <= highestLength) break
+  # Stop checking if there aren't enough characters left to beat the highest
   test <- c(t[j:(j + highestLength)])
+  # For each character, check if the following substring will beat the highest
   if(sum(duplicated(test)) == 0){
-    for(k in (j + highestLength + 1):length(t)){
+    # Only do this for highest length substrings
+    for(k in (j + highestLength + 1):l){
       test <- c(test, t[k])
+      # Add one extra character at a time
       if(sum(duplicated(test)) > 0){
         highestLength <- length(test) - 1; index <- j; break
       }
+      # When a duplicated value is found, stop checking
     }
   }
 }
 highestLength
 t[index:(index + highestLength - 1)]
 
-# Problem 4 - Median of Two Sorted Arrays
+# Problem 4 - Median of Two Sorted Arrays - Under one second
 
-a1 <- c(1,3,4,6,8,9,12,35,67,79,102); a2 <- c(2,3,4,5,6,7,8,9,10,11,23,89,105)
+# a1 <- c(1,3,4,6,8,9,12,35,67,79,102); a2 <- c(2,3,4,5,6,7,8,9,10,11,23,89,105)
+
+setwd("C:/Users/jackj.LAPTOP-U1V11TR1/Documents/R Projects")
+a1Connect <- file(description = "LeetCode4a1.txt", open = "r", blocking = TRUE)
+a1 <- readLines(a1Connect)
+a1 <- as.numeric(unlist(strsplit(a1, split = ", ")))
+a2Connect <- file(description = "LeetCode4a2.txt", open = "r", blocking = TRUE)
+a2 <- readLines(a2Connect)
+a2 <- as.numeric(unlist(strsplit(a2, split = ", ")))
 
 merged <- sort(c(a1,a2))
 
@@ -70,24 +88,27 @@ if((l %% 2) == 0){
 
 median
 
-# Problem 5 - Longest Palindromic Substring
+# Problem 5 - Longest Palindromic Substring - Under one second
 
-s <- "kajdfklsbdbgbkdkbgbjbglkbdvl"
+s <- "iohdghlabcdefghijklmnopqrstuvwxykjbnlkjfdklffffffffkdfopeifklsppowihdfdsvbsubdvhdbvjkbkjnoifebpejgvmpweflkndgkajdfklsbdbgbkdkbgbjbglkbdvllvdbklgbjbgbkdkbgbdbslplkjadggsdkljvbjvjhoiphsofdjkglgdjfkldhfkiohdghlabcdefghijklmnopqrstuvwxykjbnlkjfdklffffffffkdfopeifklsppowihdfdsvbsubdvhdbvjkbkjnoifebpejgvmpweflkndgkajdfklsbdbgbkdkbgbjbglkbdvllvdbklgbjbgbkdkbgbdbslplkjadggsdkljvbjvjhoiphsofdjkglgdjfkldhfkiohdghlabcdefghijklmnopqrstuvwxykjbnlkjfdklffffffffkdfopeifklsppowihdfdsvbsubdvhdbvjkbkjnoifebpejgvmpweflkndgkajdfklsbdbgbkdkbgbjbglkbdvllvdbklgbjbgbkdkbgbdbslplkjadggsdkljvbjvjhoiphsofdjkglgdjfkldhfkiohdghlabcdefghijklmnopqrstuvwxykjbnlkjfdklffffffffkdfopeifklsppowihdfdsvbsubdvhdbvjkbkjnoifebpejgvmpweflkndgkajdfklsbdbgbkdkbgbjbglkbdvllvdbklgbjbgbkdkbgbdbslplkjadggsdkljvbjvjhoiphsofdjkglgdjfkldhfkiohdghlabcdefghijklmnopqrstuvwxykjbnlkjfdklffffffffkdfopeifklsppowihdfdsvbsubdvhdbvjkbkjnoifebpejgvmpweflkndgkajdfklsbdbgbkdkbgbjbglkbdvllvdbklgbjbgbkdkbgbdbslplkjadggsdkljvbjvjhoiphsofdjkglgdjfkldhfk"
 
 t <- unlist(strsplit(s, split = ""))
 
+l <- length(t)
 maxLength <- 0
 index <- 0
-for(i in 1:length(t)){
-  if(length(t) - i < maxLength) break
-  for(j in i:length(t)){
-    test <- t[i:(j+maxLength)]
-    if(paste(test, collapse = "") == paste(rev(test), collapse = "")) {maxLength <- length(test); index <- i}
+for(i in 1:l){
+  if(l - i < maxLength) break
+  ends <- which(t == t[i])
+  ends <- ends[ends > (i + maxLength)]
+  for(j in ends){
+    test <- t[i:j]
+    if(!(FALSE %in% (test == rev(test)))) {maxLength <- length(test); index <- i}
   }
 }
 
 maxLength
-t[index:(index + maxLength - 1)]
+paste(t[index:(index + maxLength - 1)], collapse = "")
 
 # Problem 6 - Zigzag Conversion
 
@@ -257,7 +278,7 @@ for(i in 1:(length(test)-1)){
 sum(test)
 test
 
-# Problem 14 - Longest Common Prefix
+# Problem 14 - Longest Common Prefix - Under one second
 
 strs <- c("flower","flow","flight")
 strs2 <- c("dog","racecar","car")
@@ -301,3 +322,109 @@ commonPrefix <- function(string){
 commonPrefix(strs)
 commonPrefix(strs2)
 commonPrefix(strs3)
+
+# Problem 15 - 3Sum
+
+input <- c(1,2,3,-1,-2,-3,4,4)
+
+l <- length(input)
+winners <- list()
+for(i in 1:(l-2)){
+  for(j in (i+1):(l-1)){
+      if(-(input[i] + input[j]) %in% input[(j+1):l]){
+        winners <- append(winners, list(sort(c(input[i], input[j], -(input[i] + input[j])))))
+      }
+      # Systematically choose two values; if their inverse appears later in the input, put them all in a list
+  }
+}
+
+winners <- winners[!duplicated(winners)]
+winners
+
+# Problem 16 - 3Sum Closest
+
+nums <- c(100,5,120,1,102837,-19,3248,309785,9236598245,5,1,2,-12,-18,8)
+target <- 0
+
+l <- length(nums)
+closest <- abs((nums[1] + nums[2] + nums[3]) - target)
+# Distance away from target
+answer <- closest
+for(i in 1:(l-2)){
+  if(answer == target) break
+  for(j in (i+1):(l-1)){
+    ijDist <- target - (nums[i] + nums[j])
+    # How far away is (i + j) from the target?
+    testVals <- nums[(j+1):l]
+    # Later values to check
+    distances <- abs(testVals - ijDist)
+    # How far away each sum is from the target
+    if(min(abs(distances)) < closest){
+      closest <- abs(nums[i] + nums[j] + testVals[which(abs(distances) == min(abs(distances)))]);
+      answer <- nums[i] + nums[j] + testVals[which(abs(distances) == min(abs(distances)))]
+    }
+    # If a distance is closer to the target, update the closest value
+  }
+}
+answer
+
+# Problem 17 - Letter Combinations of a Phone Number
+
+letters <- list(c(),c("a","b","c"),c("d","e","f"),c("g","h","i"),c("j","k","l"),
+                c("m","n","o"),c("p","q","r","s"),c("t","u","v"),c("w","x","y","z"))
+digits <- "3745"
+digits <- as.numeric(unlist(strsplit(digits, split = "")))
+output <- c()
+for(i in 1:length(letters[[digits[1]]])){
+  for(j in 1:length(letters[[digits[2]]])){
+    for(k in 1:length(letters[[digits[3]]])){
+      for(l in 1:length(letters[[digits[4]]])){
+        output <- c(output, paste(c(letters[[digits[1]]][i],letters[[digits[2]]][j],letters[[digits[3]]][k],letters[[digits[4]]][l]), collapse = ""))
+      }
+    }
+  }
+}
+
+# Problem 18 - 4Sum
+
+nums <- c(1,0,-1,0,-2,2,3)
+target <- 5
+
+l <- length(nums)
+winners <- list()
+for(i in 1:(l-3)){
+  for(j in (i+1):(l-2)){
+    for(k in (j+1):(l-1)){
+      testVals <- nums[(k+1):l]
+      # Systematically choose 3 numbers, then test the rest
+      distances <- target - (nums[i] + nums[j] + nums[k] + testVals)
+      # Distance from the target when summing each number
+      if(0 %in% distances){
+        winners <- append(winners, list(sort(c(nums[i],nums[j],nums[k],(target - (nums[i] + nums[j] + nums[k]))))))
+      }
+    }
+  }
+}
+winners <- winners[!duplicated(winners)]
+# Remove duplicated answers
+winners
+
+# Problem 19 - Remove Nth Node From End of List
+
+head <- c(1,2,3,4,5); n <- 2
+head[-(length(head) - n + 1)]
+
+# Problem 20 - Valid Parentheses
+
+test <- c("(","(",")")
+length(test[test == "("])
+
+bool <- 0
+
+if(!(length(test[test == "("]) == length(test[test == ")"]))) bool <- FALSE
+if(!(length(test[test == "{"]) == length(test[test == "}"]))) bool <- FALSE
+if(!(length(test[test == "["]) == length(test[test == "]"]))) bool <- FALSE
+# There should be the same number of open/close brackets of each type
+
+
+
